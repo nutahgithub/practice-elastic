e9784eba76a9555cd49cec149064b9836760a8d1
nutahgithub <jogsellxyz@gmail.com>
2021-11-14 15:34:56 +0700

    add log

--- src/main/java/com/thanh/practiceelastic/api/server/DefaultRestApi.java
+++ src/main/java/com/thanh/practiceelastic/api/server/DefaultRestApi.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class DefaultRestApi {
+	
+	private Logger LOG = LoggerFactory.getLogger(DefaultRestApi.class);
 
 	@GetMapping(value = "/welcome")
 	public String welcome() {
-		System.out.println(StringUtils.join("ABCddd", "Spring", "Elasticsearch"));
+		LOG.info(StringUtils.join("ABCddd", "Spring", "Elasticsearch"));
 		return "Welcome spring boot";
 	}
 	
